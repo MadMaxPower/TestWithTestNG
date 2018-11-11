5ab235d31623d4f258d5a838fc131d01468d5584
Maxim <solobitches@mail.ru>
2018-11-12 01:04:21 +0300

    v 1.0

--- src/test/java/com/test/classes/TestPen.java
+++ src/test/java/com/test/classes/TestPen.java
@@ -1,166 +1,163 @@
 package com.test.classes;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import static org.mockito.Mockito.*;
-import java.util.Iterator;
+
+import java.lang.reflect.Field;
 
 public class TestPen {
 
     private Pen pen;
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp()
     {
-        //System.out.println("Метод тестируется");
+        System.out.println("System is ready for testing");
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown()
     {
-        //System.out.println("Метод протестировался");
+        System.out.println("System was tested");
     }
 
-//    @DataProvider
-//    public Object [][] shouldWriteAnythingObject()
-//    {
-//        return new Object[][]{
-//                {"Hello", pen.write("Hello")},
-//                {"", pen.write("")},
-//                {null, null},
-//        };
-//    }
-//
-//    @Test(dataProvider = "shouldWriteAnythingObject")
-//    public void testPenWrite_ShouldWriteAnything(String actual, String expected)
-//    {
-//        pen = new Pen();
-//        Assert.assertEquals(actual,expected);
-//    }
-
-//    @DataProvider
-//    public Object [][] shouldWorkingIfContainerIsNotEmpty()
-//    {
-//        return new Object[][]{
-//                {100, pen.isWork()},
-//        };
-//    }
-//
-//    @Test(dataProvider = "shouldWorkingIfContainerIsNotEmpty")
-//    public void testPenIsWork_IsPenWork(int inkContainer, Boolean res)
-//    {
-//        pen = new Pen(inkContainer);
-//        Assert.assertTrue(pen.isWork());
-//    }
-
-
-//    @Test
-//    public void testPen_constructorWithInkContainer()
-//    {
-//        int inkContainer = 100;
-//        pen = new Pen(inkContainer);
-//        //Assert.assertEquals();
-//    }
-
-    @Test
-    public void testPenGetColor_ShouldPrintRedColor()
+    @DataProvider
+    public Object [][] shouldWorkingIfContainerIsNotEmpty()
     {
-        pen = new Pen(1000,1.0,"RED");
-        Assert.assertEquals(pen.getColor(),"RED");
+        return new Object[][]{
+                {1000, pen.isWork()},
+        };
     }
 
-//    @DataProvider
-//    public Object [][] shouldWriteAnythingObject1()
-//    {
-//        return new Object[][]{
-//                {0,4,"qwertyu",""},
-//                {2.0,0,"testString",""},
-//                {5.5,4,"qwer","qwer"},
-//        };
-//    }
-//
-//    @Test(dataProvider = "shouldWriteAnythingObject1")
-//    public void testPenWrite_ShouldWriteAnything1(double sizeLetter, int inkContainer, String actual, String expected)
-//    {
-//        pen = new Pen(inkContainer, sizeLetter);
-//        Assert.assertEquals(pen.write(actual), expected);
-//    }
-
-    @Test
-    public void testPenGetColor_ShouldNotPrintColor()
+    @Test(dataProvider = "shouldWorkingIfContainerIsNotEmpty")
+    public void testPenIsWork_IsPenWork(Integer inkContainer, Boolean res)
     {
-        pen = new Pen(1000,1.0,"");
-        Assert.assertEquals(pen.getColor(),"");
+        pen = new Pen(inkContainer);
+        Assert.assertTrue(res);
     }
 
-    @Test
-    public void testPenIsWork_ShouldNotWorking()
+    @DataProvider
+    public Object [][] shouldNotWorkingIfContainerIsEmpty()
     {
-        pen = new Pen(0);
-        Assert.assertFalse(pen.isWork());
+        return new Object[][]{
+                {-1,pen.isWork()},
+                {0, pen.isWork()},
+        };
     }
 
-    @Test
-    public void testPenIsWork_ShouldWorking()
+    @Test(dataProvider = "shouldNotWorkingIfContainerIsEmpty")
+    public void testPenIsWork_IsPenWorkIfContEmpty(int inkContainer, boolean res)
     {
-        pen = new Pen(100);
-        Assert.assertTrue(pen.isWork());
-    }
-
-    @Test
-    public void testPenWrite_InkMoreThanSizeWordAndLetter0()
-    {
-        pen = new Pen(100,0);
-        Assert.assertEquals(pen.write("test"),"");
-    }
-
-    @Test
-    public void testPenWrite_InkMoreThanSizeWordAndLetter1()
-    {
-        pen = new Pen(100,1.0);
-        Assert.assertEquals(pen.write("test"),"test");
-    }
-
-    @Test
-    public void testPenWrite_InkLessThanSizeWordAndLetter1()
-    {
-        pen = new Pen(4,1.0);
-        Assert.assertEquals(pen.write("testing"),"test");
-    }
-
-    @Test
-    public void testPenWrite_InkLessThanSizeWordAndLetter0()
-    {
-        pen = new Pen(4,0);
-        Assert.assertEquals(pen.write("testing"),"");
-    }
-
-    @Test
-    public void testPenWrite_InkEqualSizeWordAndLetter0()
-    {
-        pen = new Pen(4,0);
-        Assert.assertEquals(pen.write("test"),"");
-    }
-
-    @Test
-    public void testPenWrite_InkEqualSizeWordAndLetter1()
-    {
-        pen = new Pen(4,1);
-        Assert.assertEquals(pen.write("test"),"test");
+        pen = new Pen(inkContainer);
+        Assert.assertFalse(res);
     }
 
     @Test
     public void testPenDoSomethingElse_ShouldDoSomethingElse()
     {
-//        Comparable c = mock(Comparable.class);
-//        when(c.compareTo("Test")).thenReturn(1);
-//        Assert.assertEquals(1, c.compareTo("Test"));
-        pen = new Pen(1000,1.0,"RED");
-        pen.doSomethingElse();
+        Pen pen = mock(Pen.class);
+        doNothing().when(pen).doSomethingElse();
+    }
+
+    @Test
+    public void testPenGetColor_ShouldPrintRightColor()
+    {
+        pen = new Pen(100,1.0,"RED");
+        Assert.assertEquals("RED", pen.getColor());
+    }
+
+    @Test
+    public void testPenGetColor_ShouldNotPrintColor()
+    {
+        pen = new Pen(100,1.0,"");
+        Assert.assertEquals("", pen.getColor());
+    }
+
+    @DataProvider
+    public Object [][] shouldWriteAnythingObject()
+    {
+        return new Object[][]{
+                {0,4,"qwertyu",""},
+                {-1,0,"testString",""},
+                {1,-4,"qwer",""},
+                {0,0,"qweqwe",""},
+                {1.5,4,"qwqw","qwqw"},
+        };
+    }
+
+    @Test(dataProvider = "shouldWriteAnythingObject")
+    public void testPenWrite_ShouldWriteAnything(double sizeLetter, int inkContainer, String actual, String expected)
+    {
+        pen = new Pen(inkContainer, sizeLetter);
+        Assert.assertEquals(pen.write(actual), expected);
+    }
+
+    @DataProvider
+    public Object [][] constructorWithOneParametrObject()
+    {
+        return new Object[][]{
+                {100},
+        };
+    }
+
+    @Test(dataProvider = "constructorWithOneParametrObject")
+    public void testPen_constructorWithOneParametr(Integer inkContainer) throws NoSuchFieldException, IllegalAccessException {
+        Pen pen = new Pen(inkContainer);
+
+        Field field = Pen.class.getDeclaredField("inkContainerValue");
+        field.setAccessible(true);
+        Integer value = (Integer) field.get(pen);
+        Assert.assertEquals(value, inkContainer);
+    }
+
+    @DataProvider
+    public Object [][] constructorWithTwoParametrObject()
+    {
+        return new Object[][]{
+                {100, 1.0},
+        };
+    }
+
+    @Test(dataProvider = "constructorWithTwoParametrObject")
+    public void testPen_constructorWithTwoParametr(Integer inkContainer, Double sizeLetter) throws NoSuchFieldException, IllegalAccessException {
+        Pen pen = new Pen(inkContainer, sizeLetter);
+
+        Field field = Pen.class.getDeclaredField("inkContainerValue");
+        Field field1 = Pen.class.getDeclaredField("sizeLetter");
+        field.setAccessible(true);
+        field1.setAccessible(true);
+        Integer value = (Integer) field.get(pen);
+        Double value1 = (Double) field1.get(pen);
+        Assert.assertEquals(value, inkContainer);
+        Assert.assertEquals(value1,sizeLetter);
+    }
+
+    @DataProvider
+    public Object [][] constructorWithThreeParametrObject()
+    {
+        return new Object[][]{
+                {100,1.0,"RED"},
+        };
+    }
+
+    @Test(dataProvider = "constructorWithThreeParametrObject")
+    public void testPen_constructorWithThreeParametr(Integer inkContainer, Double sizeLetter, String color) throws NoSuchFieldException, IllegalAccessException {
+        Pen pen = new Pen(inkContainer, sizeLetter, color);
+
+        Field field = Pen.class.getDeclaredField("inkContainerValue");
+        Field field1 = Pen.class.getDeclaredField("sizeLetter");
+        Field field2 = Pen.class.getDeclaredField("color");
+        field.setAccessible(true);
+        field1.setAccessible(true);
+        field2.setAccessible(true);
+        Integer value = (Integer) field.get(pen);
+        Double value1 = (Double) field1.get(pen);
+        String value2 = (String) field2.get(pen);
+        Assert.assertEquals(value, inkContainer);
+        Assert.assertEquals(value1,sizeLetter);
+        Assert.assertEquals(value2,color);
     }
 
 }
